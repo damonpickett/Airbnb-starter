9b4c1ce646161076622aa1dfbf50e67a8132d2f9
Damon Pickett <damonpickett@Damons-Mac-mini.local>
2022-04-18 11:39:29 -0700

    added google maps functionality to RentalsMap component

--- src/components/RentalsMap.js
+++ src/components/RentalsMap.js
@@ -2,21 +2,40 @@ import React from "react";
 import { Map, Marker, GoogleApiWrapper } from "google-maps-react";
 import { useState, useEffect } from 'react';
 
-function RentalsMap({locations}) {
+function RentalsMap({locations, google, setHighLight}) {
 
   const [center, setCenter] = useState();
   useEffect(() => {
     var arr = Object.keys(locations);
-    var getLat = (key) => locations[key]["lat"];
+    var getLat = (key) => locations[key]["lat"]; 
     var avgLat = arr.reduce((a, c) => a + Number(getLat(c)), 0) / arr.length;
-
+    
     var getLng = (key) => locations[key]["lng"];
     var avgLng = arr.reduce((a, c) => a + Number(getLng(c)), 0) / arr.length;
+    console.log(google)
+    setCenter({lat:avgLat, lng:avgLng})
+
   }, [locations])
 
   return (
     <>
-      <div>Map</div>
+      {center && (
+      <Map
+      google={google}
+      containerStyle={{
+        width: "50vw",
+        height: "calc(100vh - 135px)",
+      }}
+      center={center}
+      initialCenter={locations[0]}
+      zoom={13}
+      disableDefaultUI={true}
+      >
+        {locations.map((coords, i) => (
+          <Marker position={coords} onClick={() => setHighLight(i)} />
+        ))}
+      </Map>
+      )}
     </>
   );
 }
--- src/pages/Rentals.js
+++ src/pages/Rentals.js
@@ -5,9 +5,11 @@ import { useLocation } from "react-router";
 import logo from "../images/airbnbRed.png";
 import { ConnectButton, Icon, Button } from "web3uikit";
 import RentalsMap from "../components/RentalsMap";
+import { useState } from "react";
 
 const Rentals = () => {
   const {state: searchFilters} = useLocation();
+  const [highLight, setHighLight] = useState();
   const rentalsList = [
     {
       attributes: {
@@ -68,11 +70,11 @@ const Rentals = () => {
        <div className="rentalsContentL">
          Stays available for your destination
          {rentalsList &&
-         rentalsList.map(e => {
+         rentalsList.map((e, i) => {
            return(
              <>
               <hr className="line2" />
-              <div className="rentalDiv">
+              <div className={highLight == i ? "rentalDivH" : "rentalDiv"}>
                 <img className="rentalImg" src={e.attributes.imgUrl} alt='rentalImg'></img>
                 <div className="rentalInfo">
                   <div className="rentalTitle">{e.attributes.name}</div>
@@ -94,7 +96,7 @@ const Rentals = () => {
          }
        </div>
        <div className="rentalsContentR">
-         <RentalsMap locations={cords}/>
+         <RentalsMap locations={cords} setHighLight={setHighLight}/>
        </div>
      </div>
     </>
